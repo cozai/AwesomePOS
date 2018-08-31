bd120a5c4fe92c44499b8623af18f0cd3856c37d
cozai <42578376+cozai@users.noreply.github.com>
2018-08-31 22:10:30 +0800

    s

--- src/components/page/Pos.vue
+++ src/components/page/Pos.vue
@@ -129,7 +129,8 @@ export default {
   },
   created() {
     axios
-      .get("../../../static/sql/type1Goods.js")
+      // .get("../../../static/sql/type1Goods.js")
+      .get("https://cozai.github.io/AwesomePOS/static/sql/type1Goods.js")
       .then(response => {
         console.log(response);
         this.oftenGoods = response.data;
@@ -140,7 +141,7 @@ export default {
       });
 
     axios
-      .get("../../../static/sql/type2Goods.js")
+      .get("https://cozai.github.io/AwesomePOS/static/sql/type2Goods.js")
       .then(response => {
         // console.log(response);
         this.type0Goods = response.data[0];
