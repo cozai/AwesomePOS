8af01c51bd1a48be2bb74fbeb2f617a96c0335e3
cozai <42578376+cozai@users.noreply.github.com>
2018-09-25 16:36:05 +0800

    啊

--- mfresh/js/product.js
+++ mfresh/js/product.js
@@ -54,7 +54,7 @@ $(function(){
     });
     function pList(type,pageNum){
         $.ajax({
-            type:"post",
+            type:"get",
             url:"data/product_select.php",
             data:{type:type,pageNum:pageNum},
             success:function(d){
